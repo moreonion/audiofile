617e08b59fdccef41cf3af6cd952740f81785a62
Roman Zimmermann <roman@more-onion.com>
2020-01-09 17:06:13 +0100

    Basic file upload handling with dummy data.

--- package/src/main.js
+++ package/src/main.js
@@ -11,4 +11,25 @@ if (typeof window.MediaRecorder === 'undefined') {
 
 Drupal.behaviors.audiofile = {}
 Drupal.behaviors.audiofile.attach = function (context, settings) {
+  $('.audiofile-recorder', context).each((id, element) => {
+    let form = $(element).closest('form').get(0)
+    $('<input type="button" value="upload stuff"/>').click(() => {
+      let blob = new Blob(['Hello, world!'], { type: 'text/plain' })
+      let data = new FormData()
+      data.append('files[content]', blob, 'test.wav')
+      data.append('id', element.id)
+      data.append('form_build_id', form['form_build_id'].value)
+      $.ajax({
+        type: 'POST',
+        url: '/audiofile/ajax',
+        data: data,
+        processData: false,
+        contentType: false,
+        success: (data) => {
+          $('input[name$="[fid]"]', element).val(data['fid'])
+          $('input[name$="[url]"]', element).val(data['url'])
+        }
+      })
+    }).appendTo(element)
+  })
 }
