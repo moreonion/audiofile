2953f928ed3cdfcc19a98f26d61137b5cee6fb3c
Katharina Zwinger <katharina@more-onion.com>
2020-01-13 13:23:27 +0100

    Include SVG icons in (generated) HTML.
    Makes it much easier for installations using this module to override
    icon styles (although harder to replace the SVG itself).

--- package/src/template.js
+++ package/src/template.js
@@ -4,9 +4,39 @@
  * Collects all JS generated HTML in one place.
  */
 
-export const startHTML = `<div class="control icon-start control-start"></div>`
+/* ------------ icons ---------------------------------------------- */
 
-export const recordingHTML = `<div class="control icon-stop control-stop"></div>`
+/**
+ * License for mic SVG below:
+ * Font Awesome Free 5.2.0 by @fontawesome - https://fontawesome.com
+ * License - https://fontawesome.com/license (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
+ */
+const micSVG = `
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 352 512"
+  preserveAspectRatio="xMidYMid meet"
+>
+  <path
+    id="micIcon"
+    fill="currentColor"
+    d="M176 352c53.02 0 96-42.98 96-96V96c0-53.02-42.98-96-96-96S80 42.98 80 96v160c0 53.02 42.98 96 96 96zm160-160h-16c-8.84 0-16 7.16-16 16v48c0 74.8-64.49 134.82-140.79 127.38C96.71 376.89 48 317.11 48 250.3V208c0-8.84-7.16-16-16-16H16c-8.84 0-16 7.16-16 16v40.16c0 89.64 63.97 169.55 152 181.69V464H96c-8.84 0-16 7.16-16 16v16c0 8.84 7.16 16 16 16h160c8.84 0 16-7.16 16-16v-16c0-8.84-7.16-16-16-16h-56v-33.77C285.71 418.47 352 344.9 352 256v-48c0-8.84-7.16-16-16-16z"
+  />
+</svg>`
+
+const stopSVG = `
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 10 10"
+>
+  <rect x="1" y="1" width="8" height="8" fill="currentColor" id="stopIcon" />
+</svg>`
+
+/* ------------ views ---------------------------------------------- */
+
+export const startHTML = `<div class="control icon-start control-start">${micSVG}</div>`
+
+export const recordingHTML = `<div class="control icon-stop control-stop">${stopSVG}</div>`
 
 export const playerHTML = `<div>Check and <a href="" class="control-submit">submit</a></div><audio id="player" controls preload="metadata"></audio><div><a href="" class="control-reset">Or start again?</a></div>`
 
