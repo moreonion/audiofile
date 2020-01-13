72a6dd8ef1d72d12ae58b190acc1d0b460ebecda
Roman Zimmermann <roman@more-onion.com>
2020-01-13 12:48:32 +0100

    Remove files when uploading new ones.

--- package/src/main.js
+++ package/src/main.js
@@ -13,12 +13,17 @@ Drupal.behaviors.audiofile = {}
 Drupal.behaviors.audiofile.attach = function (context, settings) {
   $('.audiofile-recorder', context).each((id, element) => {
     let form = $(element).closest('form').get(0)
+    let $fid = $('input[name$="[fid]"]', element)
+    let $signature = $('input[name$="[signature]"]', element)
+    let $url = $('input[name$="[url]"]', element)
     $('<input type="button" value="upload stuff"/>').click(() => {
       let blob = new Blob(['Hello, world!'], { type: 'text/plain' })
       let data = new FormData()
       data.append('files[content]', blob, 'test.wav')
       data.append('id', element.id)
       data.append('form_build_id', form['form_build_id'].value)
+      data.append('fid', $fid.val())
+      data.append('signature', $signature.val())
       $.ajax({
         type: 'POST',
         url: '/audiofile/ajax',
@@ -26,9 +31,9 @@ Drupal.behaviors.audiofile.attach = function (context, settings) {
         processData: false,
         contentType: false,
         success: (data) => {
-          $('input[name$="[fid]"]', element).val(data['fid'])
-          $('input[name$="[signature]"]', element).val(data['signature'])
-          $('input[name$="[url]"]', element).val(data['url'])
+          $fid.val(data['fid'])
+          $signature.val(data['signature'])
+          $url.val(data['url'])
         }
       })
     }).appendTo(element)
